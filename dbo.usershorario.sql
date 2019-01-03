9fcc6c16ab96f33450691670932789e199c60b14
FabioMartins <fabio.anjos.martins@gmail.com>
2019-01-03 17:13:37 +0000

    Relatório/Login
    Criação de Autenticação
    Continuação na construção do relatório Final

---
+++ dbo.usershorario.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[usershorario] (
+    [id]       INT        NOT NULL IDENTITY,
+    [name]     NCHAR (50) NULL,
+    [password] NCHAR (10) NULL, 
+    CONSTRAINT [PK_usershorario] PRIMARY KEY ([id])
+);
+
